0438ee08cc42a7260834043d4f0e15868161885f
Leon Antczak <L.Antczak@albynschool.org>
2025-03-19 15:26:54 +0000

    hi

---
+++ task2.py
@@ -0,0 +1,6 @@
+username = input("please enter your username:")
+password = input("please enter your password")
+if username == "ian" and password == "letmein":
+    print("logon accepted")
+else:
+    print ("password incorect")    
