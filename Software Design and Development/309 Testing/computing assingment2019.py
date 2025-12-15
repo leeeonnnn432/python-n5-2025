aea3c419d89f55bac5154f1a6706c4d253539f85
Leon Antczak <L.Antczak@albynschool.org>
2025-12-15 11:42:25 +0000

    hey

--- Software Design and Development/309 Testing/computing assingment2019.py
+++ Software Design and Development/309 Testing/computing assingment2019.py
@@ -1,15 +1,14 @@
+import random
+
 endings = ["ing","end","axe","gex","goh"]
 studentsNo = int(input("enter the number of students that need passwords"))
-
 for i in range(studentsNo):
     name = input("enter first three letters of the name ")
+    while len(name) != 3 :
+        print("invalid name")
+        name = input("enter first three letters of the name ")
+    number=random.randint(0,4)
+    username = (name+endings[number])
+    print("your username is,",username) 
 
-        while len(name)!=3
-            print("error please enter 3 characters only")
-            name=input("please enter the first three characters of the student")
-            print("thank you for entering the first three letters of the name ")
-
-end = random.choice(endings)
-username = (name,end)
-final_username.append(username)
-print(final_username)
+   
